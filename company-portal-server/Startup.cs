70bdc20b799666d178d237168650b7617d91e5d7
merima98 <merimaceranic98@gmail.com>
2021-03-15 23:40:50 +0100

    Added CORS

--- company-portal-server/Startup.cs
+++ company-portal-server/Startup.cs
@@ -28,7 +28,10 @@ namespace company_portal_server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             
             services.AddControllers();
+
+            services.AddCors(options => options.AddDefaultPolicy(builder => builder.AllowAnyOrigin()));
 
             var connection = @"Server=.;Database=company_portal;Trusted_Connection=True;";
             services.AddDbContext<company_portalContext>(options => options.UseSqlServer(connection));
@@ -49,6 +52,8 @@ namespace company_portal_server
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();  
 
             app.UseAuthorization();
 
